7094c2c2e19d599aaec931295309c98e91896fb4
Sameer <sameersultan1001@gmail.com>
2023-08-23 01:24:48 +0500

    api data integrated

--- src/component/Newsitem.js
+++ src/component/Newsitem.js
@@ -1,17 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Newsitem extends Component {
   render() {
-      let {title,description,imgUrl,newsUrl}=this.props;
+    let { title, description, imgUrl, newsUrl } = this.props;
     return (
-        <div className="card m-2" style={{width:"18rem",height:"22rem"}}>
-        <img src={imgUrl} className="card-img-top" style={{height:"8rem"}} alt="..."/>
+      <div className="card m-2" style={{ width: "18rem", height: "22rem" }}>
+        <img
+          src={imgUrl}
+          className="card-img-top"
+          style={{ height: "8rem" }}
+          alt="..."
+        />
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           <p className="card-text">{description}</p>
-          <a href={newsUrl} target='_blank' className="btn btn-primary btn-sm">Read more</a>
+          <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-dark  btn-sm">
+            Read more
+          </a>
         </div>
       </div>
-    )
+    );
   }
 }
