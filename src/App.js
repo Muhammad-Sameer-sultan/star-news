652ca5cf95901a3d0de73845122ee975fdfe132b
Sameer <sameersultan1001@gmail.com>
2023-08-25 01:51:40 +0500

    navbar click set

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React, { Component } from 'react'
-import Navbar from './component/Navbar'
-import News from './component/News'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./component/Navbar";
+import News from "./component/News";
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <Navbar/>
-        <News country='us' PageSize={15} category='sports' />
-      </div>
-    )
-  }
+export default function App() {
+  return (
+    <div>
+      <Router>
+        <Navbar />  
+        <Routes>
+          <Route exact path="/business" element={<News key="business" country="us" pageSize={15} category="business" />} />
+          <Route exact path="/entertainment" element={<News key="entertainment" country="us" pageSize={15} category="entertainment" />} />
+          
+          <Route exact path="/general" element={<News key="general" country="us" pageSize={15} category="general" />} />
+          <Route exact path="/health" element={<News key="health" country="us" pageSize={15} category="health" />} />
+          <Route exact path="/sport" element={<News key="sport" country="us" pageSize={15} category="sport" />} />
+          <Route exact path="/science" element={<News key="science" country="us" pageSize={15} category="science" />} />
+         
+        </Routes>
+      </Router>
+    </div>
+  );
 }
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,56 +1,64 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export class Navbar extends Component {
-  static propTypes = {}
+  static propTypes = {};
 
   render() {
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Top-Star News</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <Link className="navbar-brand" to="/">
+            Top-Star News
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                <Link className="nav-link" to="/business">Business</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">About</a>
-              </li>
-           
-              <li className="nav-item">
-                <a className="nav-link" href="#">Business</a>
+                <Link className="nav-link" to="/entertainment">entertainment</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Enterainment</a>
+                <Link className="nav-link" to="/general">general</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">General</a>
+                <Link className="nav-link" to="/Health">Health</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Health</a>
+                <Link className="nav-link" to="/sport">sport</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Sport</a>
+                <Link className="nav-link" to="/science">science</Link>
               </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  science
-                </a>
-              </li>
-              
             </ul>
             <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-              <button className="btn btn-outline-success" type="submit">Search</button>
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
             </form>
           </div>
         </div>
       </nav>
-    )
+    );
   }
 }
 
-export default Navbar
+export default Navbar;
